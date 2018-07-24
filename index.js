4db6f7a3c03312b7db7541caf018f38c274062bb
Gergely Imreh <imrehg@gmail.com>
2018-07-24 18:18:06 +0100

    Working batch updates and token refresh

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const capitano = require("capitano");
 const config = require("config");
 const fs = require("fs");
 const util = require("util");
-var PinejsClient = require("pinejs-client");
+const jsonfile = require("jsonfile");
+const sleep = require("sleep");
+const request = require("request");
+const PinejsClient = require("pinejs-client");
 
 const env = require("get-env")({
   staging: "staging",
@@ -22,10 +25,52 @@ const authHeader = {
 };
 const resinApi = new PinejsClient(`${config.get("apiEndpoint")}/v4/`);
 
+var refreshToken = async function() {
+  const file = `./config/${env}.json`;
+  var options = {
+    url: `${config.get("apiEndpoint")}/user/v1/refresh-token`,
+    headers: {
+      Authorization: `Bearer ${authToken}`
+    }
+  };
+  request(options, (err, resp, body) => {
+    if (err) {
+      console.log(err);
+    } else {
+      var obj = {
+        apiEndpoint: config.get("apiEndpoint"),
+        authToken: body
+      };
+      jsonfile.writeFile(file, obj, function(err) {
+        if (err) {
+          console.error(err);
+        }
+      });
+    }
+  });
+
+  // resinApi.get({
+  //     url: `/user/v1/refresh-token`,
+  //     baseUrl: config.get("apiEndpoint")
+  //   })
+  //   .then(function(response) {
+  //     if (response.status === 200) {
+  //       var obj = {
+  //         apiEndpoint: config.get("apiEndpoint"),
+  //         authToken: response.body
+  //       };
+  //       jsonfile.writeFile(file, obj, function(err) {
+  //         if (err) {
+  //           console.error(err);
+  //         }
+  //       });
+  //     }
+  //   });
+};
+
 async function query_supervisor_releases(tag, device_type) {
   var query_options = {};
   if (device_type) {
-    console.log("yeah?");
     query_options = {
       options: {
         $select: ["id", "device_type"],
@@ -62,7 +107,9 @@ async function releasepairs(from_tag, to_tag, device_type) {
     });
     if (filtered_from_release.length === 1) {
       var output = {
+        from_supervisor_tag: from_tag,
         from_supervisor_release: filtered_from_release[0].id,
+        to_supervisor_tag: to_tag,
         to_supervisor_release: t.id,
         device_type: t.device_type
       };
@@ -76,7 +123,13 @@ async function releasepairs(from_tag, to_tag, device_type) {
   });
 }
 
-async function switch_supervisor(from_tag, to_tag, device_type, verbise) {
+async function switch_supervisor(
+  from_tag,
+  to_tag,
+  device_type,
+  verbose,
+  count_only
+) {
   const combos = await releasepairs(from_tag, to_tag, device_type);
   _.forEach(combos, async function(c) {
     const device = {
@@ -84,6 +137,7 @@ async function switch_supervisor(from_tag, to_tag, device_type, verbise) {
     };
     const filter = {
       options: {
+        $select: "id",
         $filter: {
           $or: [
             {
@@ -105,107 +159,165 @@ async function switch_supervisor(from_tag, to_tag, device_type, verbise) {
         }
       }
     };
-    // console.log(JSON.stringify(_.assign(device, filter, authHeader), null, 2));
-    const devices = await resinApi.get(_.assign(device, filter, authHeader));
-    const body = {
-      body: {
-        should_be_managed_by__supervisor_release: c.to_supervisor_release
-      }
-    };
-    var patch_request = _.assign(device, filter, authHeader, body);
-    // console.log(JSON.stringify(patch_request, null, 2));
-    var response = await resinApi.patch(patch_request);
-    console.log(
-      `Switching ${c.from_supervisor_release} ->  ${
-        c.to_supervisor_release
-    } for device type '${c.device_type}':\t${response} :\t${devices.length}`
-    );
+    const ft = c.from_supervisor_tag;
+    const fr = c.from_supervisor_release;
+    const tt = c.to_supervisor_tag;
+    const tr = c.to_supervisor_release;
+    const dt = c.device_type;
+
+    if (count_only) {
+      const devices = await resinApi.get(_.assign(device, filter, authHeader));
+      const count = devices.length;
+      console.log(
+        `Candidates for ${ft} (${fr}) to ${tt} (${tr}) for type '${dt}': ${count}`
+      );
+    } else {
+      const body = {
+        body: {
+          should_be_managed_by__supervisor_release: c.to_supervisor_release
+        }
+      };
+      var patch_request = _.assign(device, filter, authHeader, body);
+      var response = await resinApi.patch(patch_request);
+      console.log(
+        `Switching ${ft} (${fr}) to ${tt} (${tr}) for type '${dt}': ${response}`
+      );
+    }
+    sleep.msleep(100);
   });
 }
 
-switch_supervisor("v6.6.0", "v6.6.0_logstream");
+async function batch_switch_supervisor(versionsfile, verbose, count_only) {
+  jsonfile.readFile(versionsfile, function(err, versions) {
+    _.forEach(versions, v => {
+      if (v.from_tag && v.to_tag) {
+        switch_supervisor(
+          v.from_tag,
+          v.to_tag,
+          v.device_type,
+          verbose,
+          count_only
+        );
+      }
+    });
+  });
+}
 
-// test();
-//
-// const someFunction = () => {
-//   console.log("yeah");
-// };
-//
-// capitano.command({
-//   signature: "switch",
-//   description: "switch",
-//   help: "switch",
-//   options: [
-//     {
-//       signature: "from",
-//       parameter: "from",
-//       boolean: false,
-//       alias: ["f"]
-//     },
-//     {
-//       signature: "to",
-//       parameter: "to",
-//       boolean: false,
-//       alias: ["t"]
-//     },
-//     {
-//       signature: "devicetype",
-//       parameter: "devicetype",
-//       boolean: false,
-//       alias: ["d"]
-//     },
-//     {
-//       signature: "verbose",
-//       boolean: true,
-//       alias: ["v"]
-//     }
-//   ],
-//   action: (params, options) => {
-//     console.log(options);
-//     if (options.from && options.to) {
-//       switch_supervisor(
-//         options.from,
-//         options.to,
-//         options.devicetype,
-//         options.verbose
-//       );
-//       // switch(options.from, options.to, options.devicetype, options.verbose)
-//     }
-//   }
-// });
-//
-// capitano.command({
-//   signature: "help",
-//   description: "output general help page",
-//   help: "output general help page",
-//   action: function() {
-//     var command, i, len, ref, results;
-//     console.log(`Usage:`);
-//     console.log("\nCommands:\n");
-//     ref = capitano.state.commands;
-//     results = [];
-//     for (i = 0, len = ref.length; i < len; i++) {
-//       command = ref[i];
-//       if (command.isWildcard()) {
-//         continue;
-//       }
-//       results.push(console.log(`\t${command.signature}\t\t\t${command.help}`));
-//     }
-//     return results;
-//   }
-// });
-//
-// capitano.command({
-//   signature: "*",
-//   action: function() {
-//     return capitano.execute({
-//       command: "help"
-//     });
-//   }
-// });
-//
-// capitano.run(process.argv, function(error) {
-//   if (error != null) {
-//     throw error;
-//   }
-// });
+const cmd_options = [
+  {
+    signature: "from",
+    parameter: "from",
+    boolean: false,
+    alias: ["f"]
+  },
+  {
+    signature: "to",
+    parameter: "to",
+    boolean: false,
+    alias: ["t"]
+  },
+  {
+    signature: "devicetype",
+    parameter: "devicetype",
+    boolean: false,
+    alias: ["d"]
+  },
+  {
+    signature: "batchfile",
+    parameter: "batchfile",
+    boolean: false,
+    alias: ["b"]
+  },
+  {
+    signature: "verbose",
+    boolean: true,
+    alias: ["v"]
+  },
+  {
+    signature: "refreshtoken",
+    boolean: true,
+    alias: ["r"]
+  }
+];
+
+capitano.command({
+  signature: "switch",
+  description: "switch",
+  help: "switch",
+  options: cmd_options,
+  action: (params, options) => {
+    if (options.from && options.to) {
+      switch_supervisor(
+        options.from,
+        options.to,
+        options.devicetype,
+        options.verbose,
+        false
+      );
+    } else if (options.batchfile) {
+      batch_switch_supervisor(options.batchfile, options.verbose, false);
+    }
+    if (options.refreshtoken) {
+      refreshToken();
+    }
+  }
+});
+
+capitano.command({
+  signature: "query",
+  description: "query",
+  help: "query",
+  options: cmd_options,
+  action: (params, options) => {
+    if (options.from && options.to) {
+      switch_supervisor(
+        options.from,
+        options.to,
+        options.devicetype,
+        options.verbose,
+        true
+      );
+    } else if (options.batchfile) {
+      batch_switch_supervisor(options.batchfile, options.verbose, true);
+    }
+    if (options.refreshtoken) {
+      refreshToken();
+    }
+  }
+});
+
+capitano.command({
+  signature: "help",
+  description: "output general help page",
+  help: "output general help page",
+  action: function() {
+    var command, i, len, ref, results;
+    console.log(`Usage:`);
+    console.log("\nCommands:\n");
+    ref = capitano.state.commands;
+    results = [];
+    for (i = 0, len = ref.length; i < len; i++) {
+      command = ref[i];
+      if (command.isWildcard()) {
+        continue;
+      }
+      results.push(console.log(`\t${command.signature}\t\t\t${command.help}`));
+    }
+    return results;
+  }
+});
+
+capitano.command({
+  signature: "*",
+  action: function() {
+    return capitano.execute({
+      command: "help"
+    });
+  }
+});
+
+capitano.run(process.argv, function(error) {
+  if (error != null) {
+    throw error;
+  }
+});
