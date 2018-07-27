8cf362efb01bdc93fd54039f78dec6f3e35674e5
Gergely Imreh <imrehg@gmail.com>
2018-07-27 16:13:22 +0100

    refactor: add tombstone pubnub message, only single device update
    Signed-off-by: Gergely Imreh <imrehg@gmail.com>

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ const jsonfile = require("jsonfile");
 const sleep = require("sleep");
 const request = require("request");
 const PinejsClient = require("pinejs-client");
+const semver = require("semver");
+const PubNub = require("pubnub");
 
+var env;
 if (process.env.NODE_ENV) {
-  const env = require("get-env")({
+  env = require("get-env")({
     staging: "staging",
     production: "production",
     devenv: "devenv"
@@ -20,8 +23,9 @@ if (process.env.NODE_ENV) {
   process.exit(1);
 }
 
+var authToken;
 try {
-  const authToken = config.get("authToken");
+  authToken = config.get("authToken");
 } catch (e) {
   console.log("Can't read authToken from the config file, bailing out.");
   process.exit(2);
@@ -34,6 +38,13 @@ const authHeader = {
   }
 };
 const resinApi = new PinejsClient(`${config.get("apiEndpoint")}/v4/`);
+
+// Setup pubnub
+pubnub = new PubNub({
+  publishKey: config.get("publishKey"),
+  subscribeKey: config.get("subscribeKey"),
+  ssl: true
+});
 
 var refreshToken = async function() {
   const file = `./config/${env}.json`;
@@ -49,7 +60,9 @@ var refreshToken = async function() {
     } else {
       var obj = {
         apiEndpoint: config.get("apiEndpoint"),
-        authToken: body
+        authToken: body,
+        publishKey: config.get("publishKey"),
+        subscribeKey: config.get("subscribeKey")
       };
       jsonfile.writeFile(file, obj, function(err) {
         if (err) {
@@ -58,26 +71,17 @@ var refreshToken = async function() {
       });
     }
   });
-
-  // resinApi.get({
-  //     url: `/user/v1/refresh-token`,
-  //     baseUrl: config.get("apiEndpoint")
-  //   })
-  //   .then(function(response) {
-  //     if (response.status === 200) {
-  //       var obj = {
-  //         apiEndpoint: config.get("apiEndpoint"),
-  //         authToken: response.body
-  //       };
-  //       jsonfile.writeFile(file, obj, function(err) {
-  //         if (err) {
-  //           console.error(err);
-  //         }
-  //       });
-  //     }
-  //   });
 };
 
+/**
+ * Get the supervisor releases for a given supervisor tag, narrowed by
+ * an optional device type
+ *
+ * @param   {string} tag
+ * @param   {string} device_type (optional)
+ *
+ * @return  {array} the list of matching supervisor releases found
+ */
 async function query_supervisor_releases(tag, device_type) {
   var query_options = {};
   if (device_type) {
@@ -107,94 +111,306 @@ async function query_supervisor_releases(tag, device_type) {
   return resinApi.get(_.assign(query, query_options));
 }
 
-async function releasepairs(from_tag, to_tag, device_type) {
-  const from_releases = await query_supervisor_releases(from_tag, device_type);
-  const to_releases = await query_supervisor_releases(to_tag, device_type);
+// async function releasepairs(from_tag, to_tag, device_type) {
+//   const from_releases = await query_supervisor_releases(from_tag, device_type);
+//   const to_releases = await query_supervisor_releases(to_tag, device_type);
+//
+//   const combos = _.map(to_releases, t => {
+//     const filtered_from_release = _.filter(from_releases, f => {
+//       return f.device_type === t.device_type;
+//     });
+//     if (filtered_from_release.length === 1) {
+//       var output = {
+//         from_supervisor_tag: from_tag,
+//         from_supervisor_release: filtered_from_release[0].id,
+//         to_supervisor_tag: to_tag,
+//         to_supervisor_release: t.id,
+//         device_type: t.device_type
+//       };
+//       return output;
+//     }
+//   });
+//   // Filter out `undefined` entries, e.g. where there's 0 or more than 1 release
+//   // among the candidate `from_releases`, as those are non-actionable.
+//   return _.filter(combos, c => {
+//     return c;
+//   });
+// }
+//
+// async function switch_supervisor(
+//   from_tag,
+//   to_tag,
+//   device_type,
+//   verbose,
+//   count_only
+// ) {
+//   const combos = await releasepairs(from_tag, to_tag, device_type);
+//   _.forEach(combos, async function(c) {
+//     const device = {
+//       resource: "device"
+//     };
+//     const filter = {
+//       options: {
+//         $select: "id",
+//         $filter: {
+//           $or: [
+//             {
+//               should_be_managed_by__supervisor_release: {
+//                 $any: {
+//                   $alias: "supervisor_release",
+//                   $expr: {
+//                     supervisor_release: { id: c.from_supervisor_release }
+//                   }
+//                 }
+//               }
+//             },
+//             {
+//               device_type: c.device_type,
+//               should_be_managed_by__supervisor_release: null,
+//               supervisor_version: from_tag.slice(1)
+//             }
+//           ]
+//         }
+//       }
+//     };
+//     const ft = c.from_supervisor_tag;
+//     const fr = c.from_supervisor_release;
+//     const tt = c.to_supervisor_tag;
+//     const tr = c.to_supervisor_release;
+//     const dt = c.device_type;
+//
+//     if (count_only) {
+//       const devices = await resinApi.get(_.assign(device, filter, authHeader));
+//       const count = devices.length;
+//       console.log(
+//         `Candidates for ${ft} (${fr}) to ${tt} (${tr}) for type '${dt}': ${count}`
+//       );
+//     } else {
+//       const body = {
+//         body: {
+//           should_be_managed_by__supervisor_release: c.to_supervisor_release
+//         }
+//       };
+//       var patch_request = _.assign(device, filter, authHeader, body);
+//       var response = await resinApi.patch(patch_request);
+//       console.log(
+//         `Switching ${ft} (${fr}) to ${tt} (${tr}) for type '${dt}': ${response}`
+//       );
+//     }
+//     sleep.msleep(100);
+//   });
+// }
 
-  const combos = _.map(to_releases, t => {
-    const filtered_from_release = _.filter(from_releases, f => {
-      return f.device_type === t.device_type;
-    });
-    if (filtered_from_release.length === 1) {
-      var output = {
-        from_supervisor_tag: from_tag,
-        from_supervisor_release: filtered_from_release[0].id,
-        to_supervisor_tag: to_tag,
-        to_supervisor_release: t.id,
-        device_type: t.device_type
-      };
-      return output;
-    }
-  });
-  // Filter out `undefined` entries, e.g. where there's 0 or more than 1 release
-  // among the candidate `from_releases`, as those are non-actionable.
-  return _.filter(combos, c => {
-    return c;
-  });
-}
-
-async function switch_supervisor(
+/**
+ * Update the supervisor on a single device
+ *
+ * @param   {string} uuid
+ * @param   {string} from_tag
+ * @param   {number} to_tag
+ * @param   {string} to_tag
+ * @param   {number} to_release
+ * @param   {string} device_type
+ * @param   {boolean} query_only
+ * @param   {boolen} verbose
+ *
+ */
+async function switch_supervisor_single(
+  uuid,
   from_tag,
+  from_release,
   to_tag,
+  to_release,
   device_type,
-  verbose,
-  count_only
+  query_only,
+  verbose
 ) {
-  const combos = await releasepairs(from_tag, to_tag, device_type);
-  _.forEach(combos, async function(c) {
-    const device = {
-      resource: "device"
-    };
-    const filter = {
+  const device_resource = {
+    resource: "device"
+  };
+  // Get device type if not specified
+  if (!device_type) {
+    const device_type_filter = {
       options: {
-        $select: "id",
+        $select: "device_type",
         $filter: {
-          $or: [
-            {
+          uuid: uuid
+        }
+      }
+    };
+    const result = await resinApi.get(
+      _.assign(device_resource, device_type_filter, authHeader)
+    );
+    if (result.length === 1) {
+      device_type = result[0].device_type;
+      if (verbose) {
+        console.log(`Device type for '${uuid}' is '${device_type}'`);
+      }
+    } else {
+      console.log(`Couldn't find device with UUID = ${uuid}`);
+      return;
+    }
+  }
+
+  // Get "from" supervisor release if not given
+  if (!from_release && from_tag) {
+    const result = await query_supervisor_releases(from_tag, device_type);
+    if (result.length === 1) {
+      from_release = result[0].id;
+      if (verbose) {
+        console.log(
+          `Supervisor release for tag '${from_tag}' and device type '${device_type}' is ${from_release}`
+        );
+      }
+    } else {
+      console.log(
+        `Couldn't find supervisor for tag '${from_tag}' and device type '${device_type}'`
+      );
+      return;
+    }
+  } else if (!from_tag) {
+    console.log(
+      `Cannot work without supervisor releases specified for UUID = ${uuid}`
+    );
+    return;
+  }
+  // The supervisor reports main semver, so a tag `'v7.4.3'` would be reported as
+  // '7.4.3' in the `supervisor_version` field of the device.
+  // This would be the case for the tag 'v7.4.3_logstream' as well
+  // With this coertion, we'll get a cleaned up version of the tag, that matches
+  // what should be reported.
+  const coerced_from_tag = semver.coerce(from_tag).version;
+
+  // Get "to" supervisor release if not given
+  if (!to_release && to_tag) {
+    const result = await query_supervisor_releases(to_tag, device_type);
+    if (result.length === 1) {
+      to_release = result[0].id;
+      if (verbose) {
+        console.log(
+          `Supervisor release for tag '${to_tag}' and device type '${device_type}' is ${to_release}`
+        );
+      }
+    } else {
+      console.log(
+        `Couldn't find supervisor for tag '${to_tag}' and device type '${device_type}'`
+      );
+      return;
+    }
+  } else if (!to_tag) {
+    console.log(
+      `Cannot work without supervisor releases specified for UUID = ${uuid}`
+    );
+    return;
+  }
+
+  // Filter for to get the right & eligible device
+  const starting_filter = {
+    options: {
+      $select: "uuid",
+      $filter: {
+        uuid: uuid,
+        $or: [
+          {
+            should_be_managed_by__supervisor_release: {
+              $any: {
+                $alias: "supervisor_release",
+                $expr: {
+                  supervisor_release: { id: from_release }
+                }
+              }
+            }
+          },
+          {
+            device_type: device_type,
+            should_be_managed_by__supervisor_release: null,
+            supervisor_version: coerced_from_tag
+          }
+        ]
+      }
+    }
+  };
+
+  const device = await resinApi.get(
+    _.assign(device_resource, starting_filter, authHeader)
+  );
+  if (device.length !== 1) {
+    console.log(`No eligible device found`);
+    return;
+  } else {
+    if (query_only) {
+      // Only find the device that we we aim to update
+      console.log(`Found matching and eligible device: ${uuid}`);
+      return;
+    } else {
+      // Run the actual update
+      const patch_body = {
+        body: {
+          should_be_managed_by__supervisor_release: to_release
+        }
+      };
+      const patch_request = _.assign(
+        device_resource,
+        starting_filter,
+        authHeader,
+        patch_body
+      );
+      const patch_response = await resinApi.patch(patch_request);
+      if (patch_response === "OK") {
+        // If the device was successfully updated, this filter should catch it
+        const crosscheck_filter = {
+          options: {
+            $select: "logs_channel",
+            $filter: {
+              uuid: uuid,
               should_be_managed_by__supervisor_release: {
                 $any: {
                   $alias: "supervisor_release",
                   $expr: {
-                    supervisor_release: { id: c.from_supervisor_release }
+                    supervisor_release: { id: to_release }
                   }
                 }
               }
-            },
-            {
-              device_type: c.device_type,
-              should_be_managed_by__supervisor_release: null,
-              supervisor_version: from_tag.slice(1)
             }
-          ]
+          }
+        };
+        const device = await resinApi.get(
+          _.assign(device_resource, crosscheck_filter, authHeader)
+        );
+        if (device.length === 1) {
+          if (device[0].logs_channel !== null) {
+            // Add tombstone log
+            var publishConfig = {
+              channel: `device-${uuid}-logs`,
+              message: [
+                {
+                  t: Date.now(),
+                  m:
+                    "Logging functionality will be disabled for old clients, please update to at least SDK vX.Y.Z or CLI vA.B.C"
+                }
+              ]
+            };
+            pubnub.publish(publishConfig, function(status, response) {
+              if (status.statusCode !== 200) {
+                console.log(
+                  `PubNub tombstone failed for UUID ${uuid} with status code '${
+                    status.statusCode
+                  }'`
+                );
+              }
+            });
+          }
+          console.log(`Supervisor patched for UUID: '${uuid}'`);
+        } else {
+          console.log(
+            `Update didn't seem to happen, device might not be eligible or has changed recently: '${uuid}'`
+          );
+          return;
         }
+      } else {
+        console.log(`Patch request didn't return OK for UUID '${uuid}'`);
       }
-    };
-    const ft = c.from_supervisor_tag;
-    const fr = c.from_supervisor_release;
-    const tt = c.to_supervisor_tag;
-    const tr = c.to_supervisor_release;
-    const dt = c.device_type;
-
-    if (count_only) {
-      const devices = await resinApi.get(_.assign(device, filter, authHeader));
-      const count = devices.length;
-      console.log(
-        `Candidates for ${ft} (${fr}) to ${tt} (${tr}) for type '${dt}': ${count}`
-      );
-    } else {
-      const body = {
-        body: {
-          should_be_managed_by__supervisor_release: c.to_supervisor_release
-        }
-      };
-      var patch_request = _.assign(device, filter, authHeader, body);
-      var response = await resinApi.patch(patch_request);
-      console.log(
-        `Switching ${ft} (${fr}) to ${tt} (${tr}) for type '${dt}': ${response}`
-      );
     }
-    sleep.msleep(100);
-  });
+  }
 }
 
 async function batch_switch_supervisor(versionsfile, verbose, count_only) {
@@ -265,42 +481,80 @@ const cmd_options = [
 capitano.command({
   signature: "switch",
   description: "switch",
-  help: "switch",
+  help:
+    "switch -f/--from -t/--to -u/--uuid -d/--devicetype -b/--batchfile -v/--verbose -r/--refreshtoken",
   options: cmd_options,
   action: (params, options) => {
-    if (options.from && options.to) {
-      switch_supervisor(
+    if (!options.verbose) {
+      options.verbose = false;
+    }
+    if (options.uuid) {
+      switch_supervisor_single(
+        options.uuid,
         options.from,
+        null,
         options.to,
+        null,
         options.devicetype,
-        options.verbose,
-        false
+        false,
+        options.verbose
       );
-    } else if (options.batchfile) {
-      batch_switch_supervisor(options.batchfile, options.verbose, false);
     }
     if (options.refreshtoken) {
       refreshToken();
     }
+    // if (options.from && options.to) {
+    //   switch_supervisor(
+    //     options.from,
+    //     options.to,
+    //     options.devicetype,
+    //     options.verbose,
+    //     false
+    //   );
+    // } else if (options.batchfile) {
+    //   batch_switch_supervisor(options.batchfile, options.verbose, false);
+    // }
+    // if (options.refreshtoken) {
+    //   refreshToken();
+    // }
   }
 });
 
 capitano.command({
   signature: "query",
   description: "query",
-  help: "query",
+  help:
+    "query -f/--from -t/--to -u/--uuid -d/--devicetype -b/--batchfile -v/--verbose -r/--refreshtoken",
   options: cmd_options,
   action: (params, options) => {
-    if (options.from && options.to) {
-      switch_supervisor(
+    // if (options.from && options.to) {
+    //   switch_supervisor(
+    //     options.from,
+    //     options.to,
+    //     options.devicetype,
+    //     options.verbose,
+    //     true
+    //   );
+    // } else if (options.batchfile) {
+    //   batch_switch_supervisor(options.batchfile, options.verbose, true);
+    // }
+    // if (options.refreshtoken) {
+    //   refreshToken();
+    // }
+    if (!options.verbose) {
+      options.verbose = false;
+    }
+    if (options.uuid) {
+      switch_supervisor_single(
+        options.uuid,
         options.from,
+        null,
         options.to,
+        null,
         options.devicetype,
-        options.verbose,
-        true
+        true,
+        options.verbose
       );
-    } else if (options.batchfile) {
-      batch_switch_supervisor(options.batchfile, options.verbose, true);
     }
     if (options.refreshtoken) {
       refreshToken();
